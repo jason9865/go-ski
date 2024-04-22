13834241d390cb39a858d4475c06eb7b0e5e5602
송준석 <jswsn1526@gmail.com>
2024-04-22 17:21:13 +0900

    :art: Feat : add fontSize

--- goski_instructor/lib/const/font_size.dart
+++ goski_instructor/lib/const/font_size.dart
@@ -1,1 +1,16 @@
-class Font {}
+// 전역으로 쓰이는 폰트 사이즈 정의
+
+// 메인화면 사용자 이름
+const TITLE_LARGE = 30;
+// 작은 타이틀, ex. 리조트 글자
+const TITLE_MEDIUM = 20;
+// inputText default
+const BODY_MEDIUM = 15;
+// 작은 폰트들, ex. 쿠폰 만료 기한
+const BODY_SMALL = 12;
+// 메인 버튼
+const LABEL_LARGE = 20;
+// 기능 버튼들
+const LABEL_MEDIUM = 15;
+// 작은 버튼들
+const LABEL_SMALL = 12;
