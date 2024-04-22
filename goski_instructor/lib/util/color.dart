580f6d61f95ba32c99731b477116d2453d0b943d
고승민 <smink95@naver.com>
2024-04-22 14:20:48 +0900

    :art: Add : color

---
+++ goski_instructor/lib/util/color.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+// 배경 색
+const GOSKI_BACKGROUND = Color(0xFFF2F6F7);
+// 버튼 - 검정
+const BUTTON_BLACK = Color(0xFF141212);
+// 밝은 회색
+const GOSKI_LIGHT_GREY = Color(0xFFE5E5E5);
+// 어두운 회색
+const GOSKI_DARK_GREY = Color(0xFFE5E5E5);
+// 점선 회색
+const GOSKI_DASH_GRAY = Color(0x33000000);
+// 라이트 핑크
+const GOSKI_LIGHT_PINK = Color(0xFFFFC8C8);
+// 다크 핑크
+const GOSKI_DARK_PINK = Color(0xFFFA9494);
+// 빨간색
+const GOSKI_RED = Color(0xFFFF0000);
+// 노란색 - 별
+const GOSKI_YELLOW = Color(0xFFFFEB00);
+// 흰색
+const GOSKI_WHITE = Color(0xFFFFFFFF);
+// 파란색
+const GOSKI_BLUE = Color(0xFF156CEE);
+// 뱃지 - 녹색
+const GOSKI_GREEN = Color(0xFF44AB67);
+// 카카오 노란색
+const KAKAO_YELLOW = Color(0xFFFEE500);
+// 네이버 페이 녹색
+const NAVER_PAY_GREEN = Color(0xFF01D757);
