8342e57910b3272250e0fc10aa273a13a2fea138
고승민 <smink95@naver.com>
2024-04-23 15:36:52 +0900

    :art: Modify : goski_switch.dart

--- goski_instructor/lib/ui/component/goski_switch.dart
+++ goski_instructor/lib/ui/component/goski_switch.dart
@@ -19,64 +19,61 @@ class _GoskiSwitchState extends State<GoskiSwitch> {
 
   @override
   Widget build(BuildContext context) {
-    double chipWidth = widget.width / widget.items.length - 1;
+    double chipWidth = widget.width / widget.items.length - 2;
 
     return Container(
       width: widget.width,
       height: 40,
-      decoration: BoxDecoration(
-          border: Border.all(), borderRadius: BorderRadius.circular(10)),
+      // decoration: BoxDecoration(
+      //     border: Border.all(), borderRadius: BorderRadius.circular(10)),
       child: Row(
         children: List.generate(widget.items.length, (index) {
           return ChoiceChip(
-              backgroundColor: selectedIndex == index ? buttonBlack : goskiWhite,
-              label: Text(
+            padding: EdgeInsets.zero,
+            labelPadding: EdgeInsets.zero,
+            backgroundColor: selectedIndex == index ? buttonBlack : goskiWhite,
+            label: Container(
+              width: chipWidth,
+              height: 40,
+              alignment: AlignmentDirectional.center,
+              child: Text(
                 widget.items[index],
-                style: selectedIndex == index ?
-                Theme.of(context).textTheme.bodyLarge!.copyWith(color: goskiWhite) : Theme.of(context).textTheme.bodyLarge,
+                style: selectedIndex == index
+                    ? Theme.of(context)
+                        .textTheme
+                        .bodyLarge!
+                        .copyWith(color: goskiWhite)
+                    : Theme.of(context).textTheme.bodyLarge,
               ),
-              selected: selectedIndex == index,
-              onSelected: (bool selected) {
-                setState(() {
-                  selectedIndex = selected ? index : selectedIndex;
-                });
-              },
-              selectedColor: buttonBlack,
-              showCheckmark: false, // 선택시 체크 표시 비활성화
-              shape: index == 0
-                  ? RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(8),
-                  bottomLeft: Radius.circular(8),
-                ),
-              )
-                  : index == widget.items.length - 1
-                  ? RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(8),
-                  bottomRight: Radius.circular(8),
-                ),
-              )
-                  : RoundedRectangleBorder(
-                borderRadius: BorderRadius.zero,
+            ),
+            selected: selectedIndex == index,
+            onSelected: (bool selected) {
+              setState(() {
+                selectedIndex = selected ? index : selectedIndex;
+              });
+            },
+            selectedColor: buttonBlack,
+            showCheckmark: false, // 선택시 체크 표시 비활성화
+            shape: index == 0
+                ? RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(8),
+                bottomLeft: Radius.circular(8),
               ),
-            );
+            )
+                : index == widget.items.length - 1
+                ? RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(8),
+                bottomRight: Radius.circular(8),
+              ),
+            )
+                : RoundedRectangleBorder(
+              borderRadius: BorderRadius.zero,
+            ),
+          );
         }),
       ),
     );
   }
-}
-
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: Center(
-        child: GoskiSwitch(
-          items: ['스키', '보드', '휴무'],
-          width: 300,
-          // width: 니가만든거(0.5, 0.5) -> 400, 400(Size객체),
-        ),
-      ),
-    ),
-  ));
 }
