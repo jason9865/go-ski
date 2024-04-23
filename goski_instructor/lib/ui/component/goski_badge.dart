8b10cf1673b0d03ddd3716af543779937c649b01
최지찬 <pnlkc@naver.com>
2024-04-23 12:21:11 +0900

    :sparkles: Feat: feat goski_badge component

--- goski_instructor/lib/ui/component/goski_badge.dart
+++ goski_instructor/lib/ui/component/goski_badge.dart
@@ -4,22 +4,31 @@ import 'package:goski_instructor/ui/component/goski_text.dart';
 
 class GoskiBadge extends StatelessWidget {
   final String text;
-  final Color color;
+  final Color backgroundColor;
+  final Color textColor;
 
   const GoskiBadge({
     super.key,
     required this.text,
-    required this.color,
+    required this.backgroundColor,
+    this.textColor = goskiWhite,
   });
 
   @override
   Widget build(BuildContext context) {
     return Chip(
       label: GoskiText(
-        text : text,
+        text: text,
         size: 12,
-        color: goskiWhite,
+        color: textColor,
       ),
+      padding:
+          const EdgeInsets.all(0),
+      elevation: 0,
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8))),
+      backgroundColor: backgroundColor,
+      side: BorderSide.none,
     );
   }
 }
