5b25ef535bd6380b7e20a558881efa4fb7020ed6
송준석 <jswsn1526@gmail.com>
2024-04-23 12:00:08 +0900

    :zap: Update : update ScreenSizeController

--- goski_instructor/lib/const/util/screen_size_controller.dart
+++ goski_instructor/lib/const/util/screen_size_controller.dart
@@ -10,8 +10,18 @@ class ScreenSizeController extends GetxController {
     height.value = newHeight;
   }
 
-  // 비율에 따라 계산된 크기 반환
-  Size getWidthByRatio(double widthRatio, double heightRatio) {
+  // 비율에 따라 계산된 Size 인스턴스 반환
+  Size getSizeByRatio(double widthRatio, double heightRatio) {
     return Size(width.value * widthRatio, height.value * heightRatio);
+  }
+
+  // 비율에 따라 계산된 너비 반환
+  double getWidthByRatio(double widthRatio) {
+    return width.value * widthRatio;
+  }
+
+  // 비율에 따라 계산된 높이 반환
+  double getHeightByRatio(double heightRatio) {
+    return height.value * heightRatio;
   }
 }
