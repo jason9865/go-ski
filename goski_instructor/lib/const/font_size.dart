164958dc1c2714dd87160981ff1461b228118046
고승민 <smink95@naver.com>
2024-04-23 17:51:48 +0900

    :sparkles: Feat : Big, Middle, Small Button

--- goski_instructor/lib/const/font_size.dart
+++ goski_instructor/lib/const/font_size.dart
@@ -1,16 +1,16 @@
 // 전역으로 쓰이는 폰트 사이즈 정의
 
 // 메인화면 사용자 이름
-const titleLarge = 30;
+const double titleLarge = 30;
 // 작은 타이틀, ex. 리조트 글자
-const titleMedium = 20;
+const double titleMedium = 20;
 // inputText default
-const bodyMedium = 15;
+const double bodyMedium = 15;
 // 작은 폰트들, ex. 쿠폰 만료 기한
-const bodySmall = 12;
+const double bodySmall = 12;
 // 메인 버튼
-const labelLarge = 20;
+const double labelLarge = 20;
 // 기능 버튼들
-const labelMedium = 15;
+const double labelMedium = 15;
 // 작은 버튼들
-const labelSmall = 12;
+const double labelSmall = 12;
---
+++ goski_instructor/lib/ui/component/goski_smallsize_button.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:goski_instructor/const/color.dart';
+import 'package:goski_instructor/const/font_size.dart';
+
+class GoskiSmallsizeButton extends StatelessWidget {
+  final double width;
+  final String text;
+  final VoidCallback onTap;
+
+  const GoskiSmallsizeButton({
+    Key? key,
+    required this.width,
+    required this.text,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(buttonBlack),
+        // 배경색 설정
+        foregroundColor: MaterialStateProperty.all(goskiWhite),
+        // 텍스트 색상 설정
+        minimumSize: MaterialStateProperty.all(Size(width * 0.25, 50)),
+        // 최소 크기 설정
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15), // 테두리 반경 설정
+        )),
+        padding: MaterialStateProperty.all(EdgeInsets.all(0)), // 패딩 설정
+      ),
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: labelLarge,
+        ),
+      ),
+    );
+  }
+}
