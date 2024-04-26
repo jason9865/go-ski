767758bc1892835037987bcdee6ee6f0df94e244
고승민 <smink95@naver.com>
2024-04-26 12:58:26 +0900

    :art: Modify : Font Size

--- goski_instructor/lib/const/font_size.dart
+++ goski_instructor/lib/const/font_size.dart
@@ -1,16 +1,15 @@
 // 전역으로 쓰이는 폰트 사이즈 정의
 
-// 메인화면 사용자 이름
-const double titleLarge = 30;
-// 작은 타이틀, ex. 리조트 글자
-const double titleMedium = 20;
-// inputText default
-const double bodyMedium = 15;
-// 작은 폰트들, ex. 쿠폰 만료 기한
-const double bodySmall = 12;
-// 메인 버튼
-const double labelLarge = 20;
-// 기능 버튼들
-const double labelMedium = 15;
-// 작은 버튼들
-const double labelSmall = 12;
+const double goskiFontXXLarge = 28;
+
+const double goskiFontXLarge = 23;
+
+const double goskiFontlarge = 18;
+
+const double goskiFontmedium = 13;
+
+const double goskiFontsmall = 10;
+
+const double goskiFontXSmall = 9;
+
+const double goskiFontXXSmall = 8;
