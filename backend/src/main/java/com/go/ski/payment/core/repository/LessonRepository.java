2a88e9aa73ca095805696fe8f92fd619f99b79ea
장승호 <31498000+jshEIT@users.noreply.github.com>
2024-04-29 16:13:28 +0900

    :bug: Fix : LessonRepository 오류 수정

--- backend/src/main/java/com/go/ski/payment/core/repository/LessonRepository.java
+++ backend/src/main/java/com/go/ski/payment/core/repository/LessonRepository.java
@@ -12,46 +12,46 @@ import com.go.ski.payment.support.dto.response.UserPaymentHistoryResponseDTO;
 
 public interface LessonRepository extends JpaRepository<Lesson, Integer> {
 	@Query("SELECT NEW com.go.ski.payment.support.dto.response.UserPaymentHistoryResponseDTO ( "
-		+ "l.user.userName, t.teamName, p.paymentDate, p.paymentStatus, c.chargeName, c.studentChargeRate, p.totalAmount, "
-		+ "lp.basicFee, lp.designatedFee, lp.peopleOptionFee, lp.levelOptionFee) "
-		+ "FROM Lesson l "
-		+ "JOIN Team t  ON t.teamId = l.team.teamId "
-		+ "JOIN LessonPaymentInfo lp ON lp.lessonId = l.lessonId "
-		+ "JOIN Payment p ON p.LessonPaymentInfo.lessonId = lp.lessonId "
-		+ "JOIN Charge c ON p.chargeId = c.chargeId "
-		// + "JOIN t.instructor i "
-		+ "WHERE l.user.userId = :userId")
+			+ "l.user.userName, t.teamName, p.paymentDate, p.paymentStatus, c.chargeName, c.studentChargeRate, p.totalAmount, "
+			+ "lp.basicFee, lp.designatedFee, lp.peopleOptionFee, lp.levelOptionFee) "
+			+ "FROM Lesson l "
+			+ "JOIN Team t  ON t.teamId = l.team.teamId "
+			+ "JOIN LessonPaymentInfo lp ON lp.lessonId = l.lessonId "
+			+ "JOIN Payment p ON p.LessonPaymentInfo.lessonId = lp.lessonId "
+			+ "JOIN Charge c ON p.chargeId = c.chargeId "
+			// + "JOIN t.instructor i "
+			+ "WHERE l.user.userId = :userId")
 	List<UserPaymentHistoryResponseDTO> findStudentPaymentHistories(Integer userId);
 
 	@Query("SELECT NEW com.go.ski.payment.support.dto.response.OwnerPaymentHistoryResponseDTO ( "
-		+ "t.teamName, l.user.userName, p.paymentDate, p.paymentStatus, c.chargeName, c.ownerChargeRate, "
-		+ "p.totalAmount, lp.basicFee, lp.designatedFee, lp.peopleOptionFee, lp.levelOptionFee) "
-		+ "FROM Team t "
-		+ "JOIN Lesson l ON l.team.teamId = t.teamId "
-		+ "JOIN LessonPaymentInfo lp ON lp.lessonId = l.lessonId "
-		+ "JOIN Payment p ON p.LessonPaymentInfo.lessonId = lp.lessonId  "
-		+ "JOIN Charge c ON p.chargeId = c.chargeId "
-		+ "WHERE t.user.userId = :userId")
+			+ "t.teamName, l.user.userName, p.paymentDate, p.paymentStatus, c.chargeName, c.ownerChargeRate, "
+			+ "p.totalAmount, lp.basicFee, lp.designatedFee, lp.peopleOptionFee, lp.levelOptionFee) "
+			+ "FROM Team t "
+			+ "JOIN Lesson l ON l.team.teamId = t.teamId "
+			+ "JOIN LessonPaymentInfo lp ON lp.lessonId = l.lessonId "
+			+ "JOIN Payment p ON p.LessonPaymentInfo.lessonId = lp.lessonId  "
+			+ "JOIN Charge c ON p.chargeId = c.chargeId "
+			+ "WHERE t.user.userId = :userId")
 	List<OwnerPaymentHistoryResponseDTO> findOwnerPaymentHistories(Integer userId);
 
 	@Query("SELECT NEW com.go.ski.payment.support.dto.response.OwnerPaymentHistoryResponseDTO ( "
-		+ "t.teamName, l.user.userName, p.paymentDate, p.paymentStatus, c.chargeName, c.ownerChargeRate, "
-		+ "p.totalAmount, lp.basicFee, lp.designatedFee, lp.peopleOptionFee, lp.levelOptionFee) "
-		+ "FROM Team t "
-		+ "JOIN Lesson l ON l.team.teamId = t.teamId "
-		+ "JOIN LessonPaymentInfo lp ON lp.lessonId = l.lessonId "
-		+ "JOIN Payment p ON p.LessonPaymentInfo.lessonId = lp.lessonId  "
-		+ "JOIN Charge c ON p.chargeId = c.chargeId "
-		+ "WHERE t.teamId = :teamId")
+			+ "t.teamName, l.user.userName, p.paymentDate, p.paymentStatus, c.chargeName, c.ownerChargeRate, "
+			+ "p.totalAmount, lp.basicFee, lp.designatedFee, lp.peopleOptionFee, lp.levelOptionFee) "
+			+ "FROM Team t "
+			+ "JOIN Lesson l ON l.team.teamId = t.teamId "
+			+ "JOIN LessonPaymentInfo lp ON lp.lessonId = l.lessonId "
+			+ "JOIN Payment p ON p.LessonPaymentInfo.lessonId = lp.lessonId  "
+			+ "JOIN Charge c ON p.chargeId = c.chargeId "
+			+ "WHERE t.teamId = :teamId")
 	List<OwnerPaymentHistoryResponseDTO> findTeamPaymentHistories(Integer teamId);
 
-    List<Lesson> findByUser(User user);
+	List<Lesson> findByUser(User user);
 
-    List<Lesson> findByInstructor(Instructor instructor);
+	List<Lesson> findByInstructor(Instructor instructor);
 
-    List<Lesson> findByTeamTeamId(int teamId);
+	List<Lesson> findByTeamTeamId(int teamId);
 
-    List<Lesson> findByTeamTeamIdAndInstructorInstructorId(int teamId, int instructorId);
+	List<Lesson> findByTeamTeamIdAndInstructorInstructorId(int teamId, int instructorId);
 
 // 나중에 추가 될 요소들 c.chargeName, c.serviceCharge, i.instructorName,
 // join할 테이블
@@ -79,3 +79,4 @@ public interface LessonRepository extends JpaRepository<Lesson, Integer> {
 				 }
 			 },
 * */
+}
