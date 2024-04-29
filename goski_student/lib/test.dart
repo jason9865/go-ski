ed1835594bd5b19452e2aca2d9ba04a6e68f762f
고승민 <smink95@naver.com>
2024-04-29 10:06:16 +0900

    :tada: Begin : Goski Student

---
+++ goski_student/lib/test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:goski_student/ui/main/u026_coupon.dart';
+
+class Test extends StatelessWidget {
+  const Test({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CouponScreen();
+  }
+}
