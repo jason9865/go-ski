90020c9f0967825810ec076267ba2c94e7cdba08
송준석 <jswsn1526@gmail.com>
2024-05-02 13:50:50 +0900

    ✨ Feat : enums

---
+++ goski_instructor/lib/const/enum/domain_name.dart
@@ -0,0 +1,15 @@
+enum DomainName {
+  NAVER,
+  KAKAO,
+}
+
+/*
+### Manual
+
+// DomainName.Naver.name으로 문자열 출력
+List<Status> DomainNameList = DomainName.values;
+for (var domain in DomainNameList) {
+  print(domain.name);
+}
+
+ */
