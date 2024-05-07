d91d0f062a8508da3cdd7a2d536cba62849f8606
고정원 <7704wjddnjs@naver.com>
2024-05-07 10:41:20 +0900

    :sparkles: Feat : 404 예외 추가

--- backend/src/main/java/com/go/ski/common/exception/CommonExceptionEnum.java
+++ backend/src/main/java/com/go/ski/common/exception/CommonExceptionEnum.java
@@ -12,7 +12,8 @@ public enum CommonExceptionEnum implements ExceptionEnum {
     INVALID_FILE_TYPE(HttpStatus.BAD_REQUEST, 400, "잘못된 파일 형식입니다."),
     IMAGE_UPLOAD_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,500,"이미지 업로드에 실패했습니다."),
     IMAGE_DELETE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,500, "이미지 삭제에 실패했습니다."),
-    NO_SUCH_ELEMENT(HttpStatus.BAD_REQUEST, 400, "존재하지 않는 유저입니다.");
+    NO_SUCH_ELEMENT(HttpStatus.BAD_REQUEST, 400, "존재하지 않는 유저입니다."),
+    NO_STATIC_RESOURCE(HttpStatus.NOT_FOUND, 404, "존재하지 않는 페이지입니다.");
 
     private final HttpStatus status;
     private final int code;
