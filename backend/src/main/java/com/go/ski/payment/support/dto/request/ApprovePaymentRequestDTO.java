d74fdb31a88904fdabd8859ae396740042cc64ba
임종율 <dlawhdfbf12@naver.com>
2024-05-07 14:54:51 +0900

    :fire: Remove : 일부 DTO Json property 어노테이션 삭제

--- backend/src/main/java/com/go/ski/payment/support/dto/request/ApprovePaymentRequestDTO.java
+++ backend/src/main/java/com/go/ski/payment/support/dto/request/ApprovePaymentRequestDTO.java
@@ -1,16 +1,11 @@
 package com.go.ski.payment.support.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import lombok.Getter;
 
 @Getter
 public class ApprovePaymentRequestDTO {
 	private String tid;
-	@JsonProperty("partner_order_id")
 	private String partnerOrderId;
-	@JsonProperty("partner_user_id")
 	private String partnerUserId;
-	@JsonProperty("pg_token")
 	private String pgToken;
 }
