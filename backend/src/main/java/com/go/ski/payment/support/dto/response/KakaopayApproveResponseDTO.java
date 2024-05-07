9d27aac016c8cd0f043ffc3ccdef9b1011a1d4de
임종율 <dlawhdfbf12@naver.com>
2024-05-07 16:06:19 +0900

    :zap: Update : 일부 DTO JSON Property 수정 및 결제 승인 단계 로직 추가

--- backend/src/main/java/com/go/ski/payment/support/dto/response/KakaopayApproveResponseDTO.java
+++ backend/src/main/java/com/go/ski/payment/support/dto/response/KakaopayApproveResponseDTO.java
@@ -10,18 +10,26 @@ import lombok.Getter;
 
 @Getter
 public class KakaopayApproveResponseDTO {
-		private String aid;
-		private String tid;
-		private String cid;
-		private String sid;
-		private String partnerOrderId;
-		private String partnerUserId;
-		private String paymentMethodType;
-		private Amount amount;
-		private CardInfo cardInfo;
-		private String itemName;
-		private String itemCode;
-		private int quantity;
-		private LocalDate createdAt;
-		private LocalDate approvedAt;
+	private String aid;
+	private String tid;
+	private String cid;
+	private String sid;
+	@JsonProperty("partner_order_id")
+	private String partnerOrderId;
+	@JsonProperty("partner_user_id")
+	private String partnerUserId;
+	@JsonProperty("payment_method_type")
+	private String paymentMethodType;
+	private Amount amount;
+	@JsonProperty("card_info")
+	private CardInfo cardInfo;
+	@JsonProperty("item_name")
+	private String itemName;
+	@JsonProperty("item_code")
+	private String itemCode;
+	private int quantity;
+	@JsonProperty("created_at")
+	private LocalDate createdAt;
+	@JsonProperty("approved_at")
+	private LocalDate approvedAt;
 }
