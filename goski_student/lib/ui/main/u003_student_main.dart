ddb3b8bc73ab3e4b79d5f87c90540ae5c6b4e285
고승민 <smink95@naver.com>
2024-05-08 19:04:37 +0900

    :bug: Fix : Merge Conflict

--- goski_student/lib/ui/main/u003_student_main.dart
+++ goski_student/lib/ui/main/u003_student_main.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
