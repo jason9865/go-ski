c45ca40bb082a494d77fd0fd330862edabfd53e1
장승호 <31498000+jshEIT@users.noreply.github.com>
2024-05-08 13:52:52 +0900

    :zap: Update : NotifcationSettingResponseDTO 수정

--- backend/src/main/java/com/go/ski/notification/support/dto/NotificationSettingResponseDTO.java
+++ backend/src/main/java/com/go/ski/notification/support/dto/NotificationSettingResponseDTO.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 @AllArgsConstructor
 public class NotificationSettingResponseDTO {
 
-    private Integer notificationTypeId;
+    private Integer notificationType;
     private Boolean status;
 
 }
