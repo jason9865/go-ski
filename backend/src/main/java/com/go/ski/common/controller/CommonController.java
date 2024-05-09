b5d8a7d65d356c7e476d2e319cd69c20107084c3
임종율 <dlawhdfbf12@naver.com>
2024-05-09 14:06:55 +0900

    common 카테고리 API 반환타입 수정

--- backend/src/main/java/com/go/ski/common/controller/CommonController.java
+++ backend/src/main/java/com/go/ski/common/controller/CommonController.java
@@ -2,11 +2,13 @@ package com.go.ski.common.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.go.ski.common.response.ApiResponse;
 import com.go.ski.common.service.CommonService;
 import com.go.ski.team.core.model.SkiResort;
 import com.go.ski.team.support.dto.ResortListDTO;
@@ -24,17 +26,17 @@ public class CommonController {
 	private final CommonService commonService;
 
 	@GetMapping("/resort")
-	public ResponseEntity<List<ResortListDTO>> getResortList() {
+	public ResponseEntity<ApiResponse<?>> getResortList() {
 
-		List<ResortListDTO> result = commonService.getResortList();
-		return ResponseEntity.ok().body(result);
+		List<ResortListDTO> response = commonService.getResortList();
+		return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(response));
 	}
 
 	@GetMapping("/certificate")
-	public ResponseEntity<List<Certificate>> getCertificate() {
+	public ResponseEntity<ApiResponse<?>> getCertificate() {
 
-		List<Certificate> result = commonService.getCertificateLsit();
-		return ResponseEntity.ok().body(result);
+		List<Certificate> response = commonService.getCertificateLsit();
+		return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(response));
 	}
 
 }
