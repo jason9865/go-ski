d2d62f31e75a1eda96e7b87bd0439003bd180e93
임종율 <dlawhdfbf12@naver.com>
2024-05-09 11:15:39 +0900

    :zap: Update: 강습 결제액 조회 API 수정

--- backend/src/main/java/com/go/ski/payment/support/dto/response/LessonCostResponseDTO.java
+++ backend/src/main/java/com/go/ski/payment/support/dto/response/LessonCostResponseDTO.java
@@ -1,8 +1,9 @@
 package com.go.ski.payment.support.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class LessonCostResponseDTO {
