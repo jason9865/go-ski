d3092f4269457c19eb350deb5ffe2af37a3651c7
장승호 <31498000+jshEIT@users.noreply.github.com>
2024-05-10 16:50:45 +0900

    :bug: Fix : 강사 프로필 이미지 업로드 로직 수정

--- backend/src/main/java/com/go/ski/user/core/controller/UserController.java
+++ backend/src/main/java/com/go/ski/user/core/controller/UserController.java
@@ -11,9 +11,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.mutable.Mutable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 
 @Slf4j
@@ -61,13 +63,15 @@ public class UserController {
     }
 
     @PostMapping("/signup/inst")
-    public ResponseEntity<ApiResponse<?>> signupInstructor(HttpServletResponse response, @ModelAttribute SignupInstructorRequestDTO signupInstructorRequestDTO) {
+    public ResponseEntity<ApiResponse<?>> signupInstructor(HttpServletResponse response,
+                                                           @ModelAttribute SignupInstructorRequestDTO signupInstructorRequestDTO,
+                                                           @RequestPart MultipartFile profileImage) {
         log.info("강사 회원가입 요청: {}", signupInstructorRequestDTO);
         if (!"INSTRUCTOR".equals(signupInstructorRequestDTO.getRole().toString())) {
             throw ApiExceptionFactory.fromExceptionEnum(UserExceptionEnum.WRONG_REQUEST);
         }
 
-        User user = userService.signupInstructor(signupInstructorRequestDTO);
+        User user = userService.signupInstructor(signupInstructorRequestDTO, profileImage);
 
         log.info("회원가입 성공");
         userService.createTokens(response, user);
