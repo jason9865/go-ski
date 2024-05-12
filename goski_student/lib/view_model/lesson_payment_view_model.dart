ef0aed3aea709727516e9d4364e002b96b4bce84
고승민 <smink95@naver.com>
2024-05-13 01:32:13 +0900

    :sparkles: Feat : Payment according to designated lesson

--- goski_student/lib/view_model/lesson_payment_view_model.dart
+++ goski_student/lib/view_model/lesson_payment_view_model.dart
@@ -1,4 +1,7 @@
 import 'package:get/get.dart';
+import 'package:goski_student/const/util/url_launch_util.dart';
+import 'package:goski_student/data/model/instructor.dart';
+import 'package:goski_student/data/model/kakao_pay.dart';
 import 'package:goski_student/data/model/reservation.dart';
 import 'package:goski_student/data/model/student_info.dart';
 import 'package:goski_student/data/repository/lesson_payment_repository.dart';
@@ -11,7 +14,21 @@ class LessonPaymentViewModel {
       BeginnerResponse beginnerResponse,
       List<StudentInfo> studentInfo,
       String requestComplain) async {
-    await lessonPaymentRepository.teamLessonPayment(
-        reservationRequest, beginnerResponse, studentInfo, requestComplain);
+    KakaoPayPrepareResponse kakaoPayPrepareResponse =
+        await lessonPaymentRepository.teamLessonPayment(
+            reservationRequest, beginnerResponse, studentInfo, requestComplain);
+    UrlLaunchUtil.launch(kakaoPayPrepareResponse.next_redirect_pc_url);
+  }
+
+  Future<void> instLessonPayment(
+      ReservationRequest reservationRequest,
+      BeginnerResponse beginnerResponse,
+      Instructor instructor,
+      List<StudentInfo> studentInfo,
+      String requestComplain) async {
+    KakaoPayPrepareResponse kakaoPayPrepareResponse =
+        await lessonPaymentRepository.instLessonPayment(reservationRequest,
+            beginnerResponse, instructor, studentInfo, requestComplain);
+    UrlLaunchUtil.launch(kakaoPayPrepareResponse.next_redirect_pc_url);
   }
 }
