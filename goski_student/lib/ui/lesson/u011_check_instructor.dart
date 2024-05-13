9e16a17a23b1e12094238f79b39b83682182cc78
최지찬 <pnlkc@naver.com>
2024-05-13 14:31:07 +0900

    :sparkles: Feat: add image enlarge dialog in check instructor screen (u011)

--- goski_student/lib/ui/lesson/u011_check_instructor.dart
+++ goski_student/lib/ui/lesson/u011_check_instructor.dart
@@ -12,6 +12,8 @@ import 'package:goski_student/data/model/lesson_list_response.dart';
 import 'package:goski_student/data/model/review_response.dart';
 import 'package:goski_student/ui/component/goski_card.dart';
 import 'package:goski_student/ui/component/goski_container.dart';
+import 'package:goski_student/ui/component/goski_modal.dart';
+import 'package:goski_student/ui/component/goski_smallsize_button.dart';
 import 'package:goski_student/ui/component/goski_sub_header.dart';
 import 'package:goski_student/ui/component/goski_text.dart';
 import 'package:goski_student/view_model/instructor_profile_view_model.dart';
@@ -283,12 +285,46 @@ class CheckInstructorScreen extends StatelessWidget {
                   itemBuilder: (context, index) {
                     return Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        width: 200,
-                        margin: const EdgeInsets.symmetric(horizontal: 5),
-                        child: Image.network(
-                          profile.value.certificates[index].certificateImageUrl,
-                          fit: BoxFit.fill,
+                      child: GestureDetector(
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return GoskiModal(
+                                title: tr('feedbackImage'),
+                                child: Column(
+                                  children: [
+                                    Image.network(
+                                      width: double.infinity,
+                                      profile.value.certificates[index]
+                                          .certificateImageUrl,
+                                    ),
+                                    SizedBox(
+                                      height: screenSizeController
+                                          .getHeightByRatio(0.025),
+                                    ),
+                                    GoskiSmallsizeButton(
+                                      width: screenSizeController
+                                          .getWidthByRatio(3),
+                                      text: tr('confirm'),
+                                      onTap: () {
+                                        Navigator.pop(context);
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                          );
+                        },
+                        child: Container(
+                          width: 200,
+                          margin: const EdgeInsets.symmetric(horizontal: 5),
+                          child: Image.network(
+                            profile
+                                .value.certificates[index].certificateImageUrl,
+                            fit: BoxFit.fill,
+                          ),
                         ),
                       ),
                     );
