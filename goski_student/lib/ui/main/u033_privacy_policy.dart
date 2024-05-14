294c3431c202093f2086a847a5db99b871a5c9fd
고승민 <smink95@naver.com>
2024-05-14 12:28:08 +0900

    :memo: Add : Privacy Policy, Service Terms

--- goski_student/lib/ui/main/u033_privacy_policy.dart
+++ goski_student/lib/ui/main/u033_privacy_policy.dart
@@ -1,45 +1,59 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:flutter/services.dart';
 import 'package:goski_student/const/color.dart';
 import 'package:goski_student/const/font_size.dart';
-import 'package:goski_student/const/util/screen_size_controller.dart';
 import 'package:goski_student/ui/component/goski_sub_header.dart';
 import 'package:goski_student/ui/component/goski_text.dart';
-import 'package:logger/logger.dart';
 
-final Logger logger = Logger();
-final screenSizeController = Get.find<ScreenSizeController>();
-
-// TODO: 개인정보처리방침 페이지  추가 필요
-class PrivacyPolicyScreen extends StatelessWidget {
-
+class PrivacyPolicyScreen extends StatefulWidget {
   const PrivacyPolicyScreen({super.key});
 
   @override
+  _PrivacyPolicyScreenState createState() => _PrivacyPolicyScreenState();
+}
+
+class _PrivacyPolicyScreenState extends State<PrivacyPolicyScreen> {
+  late Future<String> _privacyPolicyContent;
+
+  @override
+  void initState() {
+    super.initState();
+    _privacyPolicyContent = loadPrivacyPolicy();
+  }
+
+  Future<String> loadPrivacyPolicy() async {
+    return await rootBundle
+        .loadString('assets/policy/GOSKI_Privacy_Policy.txt');
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Builder(builder: (context) {
-      return Scaffold(
-        appBar: GoskiSubHeader(title: tr('privacyPolicy')),
-        body: Container(
-          decoration: const BoxDecoration(
-            color: goskiBackground,
-          ),
-          child: const Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Expanded(
-                child: Center(
-                  child: GoskiText(
-                    text: '개인정보처리방침 페이지',
-                    size: goskiFontXXLarge,
-                  ),
-                ),
-              )
-            ],
-          ),
+    return Scaffold(
+      appBar: GoskiSubHeader(title: tr('privacyPolicy')),
+      body: Container(
+        decoration: const BoxDecoration(
+          color: goskiBackground,
         ),
-      );
-    });
+        child: FutureBuilder<String>(
+          future: _privacyPolicyContent,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              }
+              return SingleChildScrollView(
+                  padding: const EdgeInsets.all(16.0),
+                  child: GoskiText(
+                    text: snapshot.data ?? 'No policy available.',
+                    size: goskiFontSmall,
+                  ));
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
+      ),
+    );
   }
 }
